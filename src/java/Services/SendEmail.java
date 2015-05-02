4caa768d9c7845f1ea9e6842361c8b806dd42221
felipegi91 <felipegi91@gmail.com>
2015-05-02 18:13:44 -0500

    Email html and plain text

--- src/java/Services/SendEmail.java
+++ src/java/Services/SendEmail.java
@@ -50,4 +50,45 @@ public class SendEmail
       }
    }
       
+   public static void sendHTMLemail(String to, String from, String channel, String url, String text )
+   {
+      // Assuming you are sending email from localhost
+      String host = "localhost";
+
+      // Get system properties
+      Properties properties = System.getProperties();
+
+      // Setup mail server
+      properties.setProperty("mail.smtp.host", host);
+
+      // Get the default Session object.
+      Session session = Session.getDefaultInstance(properties);
+
+      try{
+         // Create a default MimeMessage object.
+         MimeMessage message = new MimeMessage(session);
+
+         // Set From: header field of the header.
+         message.setFrom(new InternetAddress(from));
+
+         // Set To: header field of the header.
+         message.addRecipient(Message.RecipientType.TO,
+                                  new InternetAddress(to));
+
+         // Set Subject: header field
+         message.setSubject("This is the Subject Line!");
+
+         // Send the actual HTML message, as big as you like
+         message.setContent("<h1>New content!</h1><span>On <b>"+channel+"<b></span>"
+                 + "<p>"+text+"</p><img style='width: 300px, height: auto'src='"+url+"'/>",
+                            "text/html" );
+
+         // Send message
+         Transport.send(message);
+         System.out.println("Sent message successfully....");
+      }catch (MessagingException mex) {
+         mex.printStackTrace();
+      }
+   }
+
 }
